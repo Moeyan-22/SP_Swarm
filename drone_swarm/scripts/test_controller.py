0a08c3b16c3812b562b11d771fe6ccdb255d0e7c
Moeyan-22 <coolmoehtut@gmail.com>
2024-02-27 16:33:09 +0800

    EDIT: created text controller and driver; got drone to detect mpad

---
+++ drone_swarm/scripts/test_controller.py
@@ -0,0 +1,163 @@
+#! /usr/bin/env python3
+
+import rospy
+import queue
+import numpy as np
+import json
+from std_msgs.msg import String
+from std_msgs.msg import Int32
+from geometry_msgs.msg import Point
+
+
+
+class DroneController:
+
+    """
+    single Tello Edu Controller written by Moeyan-22
+
+    Contains Utility function for
+     - Sending commands to tello driver
+     - Creates Queue for commands that is executed every 2secs
+     - Listen and respond to takeoff command published in group topic
+     - Listen to seqeunce_command and starts only when index has been called
+     - Listens to uwb point message from appropriate topic
+     - Calculates and sends path finding commands
+     - sends "state?" commands alongside any message in queue
+
+    Launch Params:
+     - name 
+     - id
+     - group
+     - target (1D array)
+    
+    """
+
+    def __init__(self):
+
+        rospy.init_node('drone_controller', anonymous=True)
+        self.cmd_queue = queue.Queue()
+
+        self.name = rospy.get_param('~name', 'tello')
+        self.id = rospy.get_param('~id', 0)
+        self.group = rospy.get_param('~group', 'A')
+        self.target_raw_string = rospy.get_param('~target', '[]')
+        self.target_raw = json.loads(self.target_raw_string)
+        self.ns = self.name + str(self.id)
+
+        self.target = []
+        self.takeoff = 0
+        self.started = False
+        self.current_sequence = 0
+        self.x = 0
+        self.y = 0
+        self.total_steps = 10
+        self.step = 1
+        self.step_interval = rospy.Rate(1/0.1) 
+        self.base_speed = 0.3
+        self.max_pos_error = 0.1
+        self.max_movement = 100
+        
+        self.command_pub = rospy.Publisher('/{}/cmd'.format(self.ns), String, queue_size=10)
+        self.uwb_sub = rospy.Subscriber('/{}/uwb'.format(self.ns), Point, self.get_uwb, queue_size=10)
+        self.takeoff_sub = rospy.Subscriber('/{}/takeoff_command'.format(self.group), Int32, self.get_takeoff_command, queue_size=10)
+        self.sequence_sub = rospy.Subscriber('/{}/sequence_command'.format(self.group), Int32, self.get_sequence, queue_size=10)
+
+
+    def get_takeoff_command(self, data):
+        if self.takeoff == 0:
+            self.takeoff = data.data
+        elif self.takeoff == 1:
+            self.execute_takeoff()
+            self.takeoff += 1
+
+
+    def execute_takeoff(self):
+        self.cmd_queue.put("command")
+        self.cmd_queue.put("mon")
+        self.cmd_queue.put("takeoff")
+
+    def get_uwb(self, data):
+        self.x = data.x
+        self.y = data.y
+
+    def process_target_raw(self):
+        i = 0
+        cycles = self.target_raw[0]
+        self.target = [[0, 0] for _ in range(cycles)]
+        while i < cycles:
+            self.target[i][0] = self.target_raw[(i + 1)*2-1]
+            self.target[i][1] = self.target_raw[(i + 1)*2]
+            i += 1
+        # self.positioning()
+        
+    def positioning(self):
+        if self.started:
+            for target in self.target:
+
+                point = Point()
+                point.x = 0
+                point.y = 0
+                rc_command = ""
+
+                point1 = np.array(target[0])
+                point2 = np.array(target[1])
+
+                diff = point2 - point1
+                dist = np.linalg.norm(diff)
+                speed_multiplier = self.base_speed
+
+                if dist < self.max_pos_error:
+                    speed_multiplier *= 0.3
+
+                [point.x, point.y] = speed_multiplier * diff / dist
+                [point.x, point.y] = [point.x, point.y] * self.max_movement
+                rc_command = "rc {} {} 0 0".format(point.x, point.y)
+                self.cmd_queue.put(rc_command)
+
+
+    def get_sequence(self, data):
+        self.current_sequence = data.data
+        self.check_seqeunce()
+
+    def check_seqeunce(self):
+        if self.current_sequence == self.id and self.started == False:
+            self.started = True
+        
+
+    def timing_control(self):
+
+        i = 0
+
+        while not rospy.is_shutdown():
+            if not self.cmd_queue.empty() and i == 0:
+                cmd = self.cmd_queue.get()
+                self.command_pub.publish(cmd)
+                i += self.step
+                self.step_interval.sleep()
+            else:
+                self.command_pub.publish("mid?")
+                i += self.step
+                self.step_interval.sleep()
+
+            while i < self.total_steps and i != 0:
+                self.command_pub.publish("mid?")
+                i += self.step
+                self.step_interval.sleep()
+
+                if i == self.total_steps:
+                    i = 0
+                    
+
+    def start(self):
+        self.timing_control()
+        self.process_target_raw()
+
+
+if __name__ == '__main__':
+    try:
+        tello_controller = DroneController()
+        tello_controller.start()
+    except rospy.ROSInterruptException:
+        rospy.logerr("ROS node interrupted.")
+    except Exception as e:
+        rospy.logerr("Error at Tello controller: {}".format(e))
---
+++ drone_swarm/scripts/test_driver.py
@@ -0,0 +1,165 @@
+#! /usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+from drone_swarm.msg import Array
+
+
+import socket
+import threading
+
+class DroneNode:
+
+    """
+    
+    Single Tello Edu Driver written by Moeyan-22
+
+    Containts Utility function for
+     - executing command
+     - listens for responses from drone/sim
+     - checking mpad id
+     - mpad logic
+     - publishes actions taken for sim
+
+    Launch Params:
+     - name
+     - id
+     - drone ip
+     - local port
+    
+    *Note commads such as 'mid?' and 'takeoff' is sent through tello_controller
+
+    
+    """
+
+    def __init__(self):
+
+
+        rospy.init_node('drone', anonymous=True)
+        
+        self.name = rospy.get_param('~name', 'tello')
+        self.id = rospy.get_param('~id', 0)
+        self.drone_ip = rospy.get_param('~drone_ip', '192.168.0.101')
+        self.local_port = rospy.get_param('~local_port', 9010)
+        self.ns = self.name + str(self.id)
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(('', self.local_port))
+        self.response = ''
+        self.mpad = 0
+        self.known_mpad = []
+        self.rate = rospy.Rate(20)
+        self.rescue = False
+
+        self.action_pub = rospy.Publisher('/{}/action'.format(self.ns), String, queue_size=10)
+        self.mpad_pub = rospy.Publisher('mpad', Array, queue_size=10)
+        self.mpad_sub = rospy.Subscriber('mpad', Array, self.get_mpad, queue_size=10)
+        self.command_sub = rospy.Subscriber('/{}/cmd'.format(self.ns), String, self.send_command, queue_size=10)
+
+    def send(self, message = ''):
+        try:
+            self.sock.sendto(message.encode(), (self.drone_ip, 8889))
+        except Exception as e:
+            rospy.logerr("Error sending message: " + str(e))
+
+    def send_command(self, command = ''):
+        if self.rescue == False:
+            rospy.loginfo(f"executing command: {command.data}")
+            self.send(command.data)
+            self.action_pub.publish(command)
+        else:
+            self.rescue_action()
+
+    def rescue_action(self):
+        self.send('stop')
+        self.send('go 100 0 0 10 {}'.format(self.mpad))
+
+    def check(self, message):
+        if message != 'ok' and message != 'error':
+            self.mpad = message.find('mpad:')
+            if self.mpad != -1:
+                self.mpad = int(message[self.mpad + 5])
+ 
+
+    def receive_command(self):
+        while not rospy.is_shutdown():
+            self.response, _ = self.sock.recvfrom(128)
+            self.response = self.response.decode(encoding='utf-8').strip()
+            rospy.loginfo(f"Received reply from drone: {self.response}")
+            self.check(self.response)
+            self.rate.sleep()
+
+    def get_mpad(self, data):
+        self.known_mpad = data
+
+    def update_mpad(self):
+        while not rospy.is_shutdown():
+            for mpad in self.known_mpad:
+                if self.mpad != -1:
+                    self.check_mpad(mpad)
+                else:
+                    pass
+
+            self.rate.sleep()
+
+    def check_mpad(self, mpad):
+
+        special = False
+        rescured = False
+
+        if self.mpad != mpad:
+            self.rescue = True
+            self.known_mpad.append(self.mpad)
+            self.mpad_pub.publish(self.known_mpad)
+        elif self.mpad == mpad:
+            special = self.check_special(mpad)
+            if special == False:
+                pass
+            elif special == True:
+                rescured = self.check_rescured(mpad)
+                if rescured == False:
+                    self.rescue = True
+                    self.known_mpad.append(self.mpad)
+                    self.mpad_pub.publish(self.known_mpad)
+                elif rescured == True:
+                    pass
+    
+
+    def check_special(self, mpad):
+        if (mpad % 2 == 0) :
+            return True
+        else:
+            return False
+        
+    def check_rescured(self, mpad):
+
+        count = 0
+
+        count = self.known_mpad.count(mpad)
+        if count == 1:
+            return False
+        elif count == 2:
+            return True
+        else:
+            rospy.logerr("Unexpected count value: {}".format(count))
+            return False        
+
+
+    def start(self):
+        # Start the receive_response thread
+        response_thread = threading.Thread(target=self.receive_command())
+        response_thread.start()
+
+        self.update_mpad
+
+        # Spin until the node is shut down
+        rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        drone = DroneNode()
+        drone.start()
+    except rospy.ROSInterruptException:
+        rospy.logerr("ROS node interrupted.")
+    except Exception as e:
+        rospy.logerr("Error at Tello driver: {}".format(e))
